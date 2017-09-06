7082b1f2a7b68e7841e2742b1e38aaf39ce59b31
umschaudhary <umesschaudhary@gmail.com>
2017-09-06 22:17:58 +0545

    code aligned and improved functions

--- pass-surakshya.py
+++ pass-surakshya.py
@@ -1,18 +1,14 @@
-
 from ast import literal_eval
 import os
-from datetime import date
-from datetime import time
 from datetime import datetime
 import pyperclip as pc
 from getpass import getpass
 from termcolor import colored
 
-
 dict = {}
 file = ".file.txt"
 path = os.path.join(os.path.expanduser("~"))
-today=datetime.now()
+today = datetime.now()
 
 
 def search(file, path):
@@ -51,82 +47,78 @@ def startup():
 
 
 def user():
-    print(colored("===================================","blue"))
-    print(colored("===================================","blue"))
-    print(colored(" Date and Time is :%s" % today,"green"))
+    print(colored("=====WELCOME TO PASSURAKSHYA======", "green"))
+    print(colored("===================================", "blue"))
+    print(colored("===================================", "blue"))
+    print(colored(" Date and Time is :%s" % today, "green"))
     dict = startup()
-    print(colored("\t1.Save Username and password","yellow"))
-    print(colored("\t2.Retrieve password","yellow"))
-    print(colored("\t3.Exit","yellow"))
-    print(colored("===================================","blue"))
-    print(colored("===================================","blue"))
-    choice = int(input(colored(" Enter your choice : ","yellow")).strip())
+    print(colored("\t1.Save Username and password", "yellow"))
+    print(colored("\t2.Retrieve password", "yellow"))
+    print(colored("\t3.Exit", "yellow"))
+    print(colored("===================================", "blue"))
+    print(colored("===================================", "blue"))
+    choice = int(input(colored(" Enter your choice : ", "yellow")).strip())
     while True:
         if choice == 1:
 
-            username = input(colored(" Enter username : ","yellow").strip())
+            username = input(colored(" Enter username : ", "yellow").strip())
             while username == "":
-                username = input(colored(" UserName Required : ","red").strip())
+                username = input(colored(" UserName Required : ", "red").strip())
             while username in dict:
-            	print(colored("***Username exits*** ","red"))
-            	username = input(colored(" Enter username : ","yellow").strip())
-            	
-            
+                print(colored("***Username exits*** ", "red"))
+                username = input(colored(" Enter username : ", "yellow").strip())
 
             password = getpass(prompt=' Enter Password :').strip()
             while password == "":
                 password = getpass(prompt=' Password Required: ').strip()
-            cpassword=getpass(prompt=' Confirm Password : ').strip()
+            cpassword = getpass(prompt=' Confirm Password : ').strip()
 
-            while password!=cpassword:
-                print(colored("** Password Donot matched ***","red"))
+            while password != cpassword:
+                print(colored("** Password Don't matches ***", "red"))
                 password = getpass(prompt=' Enter Password : ').strip()
-                cpassword=getpass(prompt=' Confirm Password : ').strip()
-                
+                cpassword = getpass(prompt=' Confirm Password : ').strip()
 
-            
             dict = save(username, password)
-            print(colored("Username and Password saved ","green"))
-            while input(colored(" Save Another one (y/n): ","yellow")).lower() == 'y':
+            print(colored("Username and Password saved ", "green"))
+            while input(colored(" Save Another one (y/n): ", "yellow")).lower() == 'y':
                 break
             else:
-                print(colored("\n\n=======================","blue"))
-                print(colored("1. Retrieve Your Password","yellow"))
-                print(colored("2. Exit ","yellow"))
-                print(colored("\n=======================","blue"))
+                print(colored("\n\n=======================", "blue"))
+                print(colored("1. Retrieve Your Password", "yellow"))
+                print(colored("2. Exit ", "yellow"))
+                print(colored("\n=======================", "blue"))
 
-                xchoice = input(colored("\n Your Choice : ","yellow"))
+                xchoice = input(colored("\n Your Choice : ", "yellow"))
                 if int(xchoice) == 1:
                     choice = 2
 
                 elif int(xchoice) == 2:
-                    print(colored(" !! Thank you !!\n ","green"))
+                    print(colored(" !! Thank you !!\n ", "green"))
                     exit()
 
         elif choice == 2:
             dict = get_directory()
-            ask = input(colored(" Enter username : ","yellow")).strip()
+            ask = input(colored(" Enter username : ", "yellow")).strip()
             while ask not in dict:
-            	print (colored( "Username Not Found !","red"))
-            	ask = input(colored("Enter Username : ","yellow")).strip()
+                print (colored("Username Not Found !", "red"))
+                ask = input(colored("Enter Username : ", "yellow")).strip()
 
             passs = dict[ask]
-            print(colored(" Password For : %s : copied to clipboard . " % ask,"green"))
-            print(colored(" You Can Paste using 'ctrl+v' Where You Want","green"))
+            print(colored(" Password For : %s : copied to clipboard . " % ask, "green"))
+            print(colored(" You Can Paste using 'ctrl+v' Where You Want", "green"))
             try:
                 pc.copy(passs)
             except Exception:
                 print(Exception)
-            
-                
-            if input(colored(" Retrive Another Password (y/n) :  ","yellow")).lower() == "y":
+
+            if input(colored(" Retrive Another Password (y/n) :  ", "yellow")).lower() == "y":
                 choice = 2
             else:
                 user()
 
         else:
-        	print(colored("!!!Thank you !!!\n","green"))
-        	exit()
+            print(colored("!!!Thank you !!!\n", "green"))
+            exit()
 
 
 user()
