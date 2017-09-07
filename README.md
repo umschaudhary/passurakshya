24b09604650b4c53a511b0ce6a34c795eb6052fd
umschaudhary <umesschaudhary@gmail.com>
2017-09-08 01:09:43 +0545

    passurakshya v2.0.1

--- README.md
+++
@@ -1,4 +0,0 @@
-A python project to save username and password . And retrive it later on a different manner . It doesnot shows the password in fact it copies your password to the clipboard ,so that you can paste it anywhere for your convinence . 
- However you will need pyperclip to be installed on your computer
-you will need xclip installed on your computer 
-Thanks 
--- setup.py
+++ setup.py
@@ -3,17 +3,14 @@ try:
 except ImportError:
     from distutils.core import setup
 
-requirements = [pkg.split('=')[0] for pkg in open('requirements.txt').readlines()]
-
 setup(name='passurakshya',
-      version='1.0',
+      version='2.0.1',
       description="Save your passwords even safer : ",
       author='Umesh Chaudhary',
       author_email='umesschaudhary@gmail.com',
       url='https://github.com/umschaudhary/passurakshya',
       scripts=['src/passurakshya'],
-      install_requires=requirements,
-      packages=['pasurakshya'],
+      packages=['passurakshya'],
       package_dir = {'passurakshya': 'src/pasurakshya'},
       classifiers=['Environment :: Console',
                'Programming Language :: Python :: 3' ]
